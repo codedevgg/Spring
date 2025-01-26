e6a93ae90c8393a501ad40ab67ad060863bc0a81
codedevgg <ginogrecor@hotmail.com>
2025-01-26 11:15:21 -0400

    wiringBeans

--- src/main/java/com/example/config/ProjectConfig.java
+++ src/main/java/com/example/config/ProjectConfig.java
@@ -1,5 +1,6 @@
 package com.example.config;
 
+import com.example.beans.Person;
 import com.example.beans.Vehicle;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,19 +14,20 @@ public class ProjectConfig {
     value to the context.
      **/
     @Bean
-    Vehicle vehicle(){
-        var veh = new Vehicle();
-        veh.setName("Audi 8");
-        return veh;
+    public Vehicle vehicle(){
+        Vehicle vehicle = new Vehicle();
+        vehicle.setName("Toyota");
+        return vehicle;
     }
     @Bean
-    String hello(){
-        return "Hello World";
+    public Person person(){
+        Person person = new Person();
+        person.setName("Toty");
+        person.setVehicle(vehicle());
+        return person;
     }
-    @Bean
-    Integer number(){
-        return 16;
-    }
+
+
 
 
 }
